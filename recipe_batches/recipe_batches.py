052d04a7cf598e8b06edf6e5c920ab4d8274c943
Michael Mangus <mjmangus16@yahoo.com>
2019-12-11 17:06:03 -0500

    completed stock prices and recipe batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,35 @@
 
 import math
 
-def recipe_batches(recipe, ingredients):
-  pass 
 
+def recipe_batches(recipe, ingredients):
+    cont = {}
+
+    batches = 0
+
+    for key_r in recipe:
+        if key_r in ingredients:
+            batches = 1
+        else:
+            batches = 0
+            return batches
+
+    for key_i in ingredients:
+        cont.update({key_i: 0})
+        cont[key_i] = ingredients[key_i] // recipe[key_i]
+
+    batches = min(cont.values())
+
+    print(cont)
+    return batches
+
+
+print(recipe_batches({"milk": 5, "honey": 5}, {"milk": 15, "honey": 14}))
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,14 +2,29 @@
 
 import argparse
 
-def find_max_profit(prices):
-  pass
 
+def find_max_profit(prices):
+
+    cur_max = -5000
+
+    for index, b in enumerate(prices):
+        for s in range(index+1, len(prices)):
+
+            if prices[s] - b > cur_max:
+                cur_max = prices[s] - b
+
+    return cur_max
+
+
+print(find_max_profit([32, 16, 8, 4, 2]))
 
 if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+    # This is just some code to accept inputs from the command line
+    parser = argparse.ArgumentParser(
+        description='Find max profit from prices.')
+    parser.add_argument('integers', metavar='N', type=int,
+                        nargs='+', help='an integer price')
+    args = parser.parse_args()
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+    print("A profit of ${profit} can be made from the stock prices {prices}.".format(
+        profit=find_max_profit(args.integers), prices=args.integers))
